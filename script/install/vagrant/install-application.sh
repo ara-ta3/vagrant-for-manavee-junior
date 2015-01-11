475d066834ea5d38305569c25f29aaf04587188f
Arata <tarata43@yahoo.co.jp>
2015-01-11 21:13:47 +0900

    expectが動かないことの対応
    interactが無いとダメだった

--- script/install/vagrant/install-application.sh
+++ script/install/vagrant/install-application.sh
@@ -1,7 +1,9 @@
+password=`cat /vagrant/script/keys/password_for_private_key`
 expect -c "
-spawn git clone git@github.com:tarata/manavee-for-junior.git /web/httpd_manavee/manavee-for-junior
+rspawn git clone git@github.com:tarata/manavee-for-junior.git /web/httpd_manavee/manavee-for-junior
 expect \"Enter passphrase for key '/home/vagrant/.ssh/private_key_for_github':\"
-send \"`cat /vagrant/script/keys/password_for_private_key`\n\"
+send \"${password}\n\"
+interact
 "
 make init -C /web/httpd_manavee/manavee-for-junior
 make install -C /web/httpd_manavee/manavee-for-junior
