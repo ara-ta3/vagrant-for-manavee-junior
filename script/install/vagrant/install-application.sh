7d13b87c2d1363f7bc84d974ef05a24346754a22
Arata <tarata43@yahoo.co.jp>
2015-01-11 20:29:39 +0900

    password対策

--- script/install/vagrant/install-application.sh
+++ script/install/vagrant/install-application.sh
@@ -1,3 +1,8 @@
-git clone git@github.com:tarata/manavee-for-junior.git /web/httpd_manavee/manavee-for-junior
+#!/usr/bin/expect
+
+spawn git clone git@github.com:tarata/manavee-for-junior.git /web/httpd_manavee/manavee-for-junior
+expect "Enter passphrase for key '/home/vagrant/.ssh/private_key_for_github':"
+send "`cat /vagrant/script/keys/password_for_private_key`\n"
+
 make init -C /web/httpd_manavee/manavee-for-junior
 make install -C /web/httpd_manavee/manavee-for-junior
