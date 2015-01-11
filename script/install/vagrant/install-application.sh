0ac9f669296a2de16787cda7595191f8651eadc0
Arata <tarata43@yahoo.co.jp>
2015-01-11 21:31:31 +0900

    timeoutなどの設定

--- script/install/vagrant/install-application.sh
+++ script/install/vagrant/install-application.sh
@@ -1,8 +1,10 @@
 password=`cat /vagrant/script/keys/password_for_private_key`
 expect -c "
+set timeout -1
 spawn git clone git@github.com:tarata/manavee-for-junior.git /web/httpd_manavee/manavee-for-junior
 expect \"Enter passphrase for key '/home/vagrant/.ssh/private_key_for_github':\"
 send \"${password}\n\"
+expect \"#\"
 interact
 "
 make init -C /web/httpd_manavee/manavee-for-junior
